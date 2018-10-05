ef7a6f7afb339a94cda185efea94ff326304e658
cristian clavijo <cristian982940@gmail.com>
2018-10-04 23:50:46 -0500

    Version de responsive a tablets y monitores
    Esta version contiene la pagina web adaptable para monitores de 1280 a 1680, y para tablets de 1279 a 768

--- JS/index.js
+++ JS/index.js
@@ -10,46 +10,59 @@
 $(document).ready(function(){ /*Bloque de codigo JQUERY*/
 
 /*==========Funcionamiento con animacion de los botones de la portada======*/
+	
 	$("#Habilidades").on("click",function(e){
 		e.preventDefault();
 		animacion("#contenedorHabilidades");
+		$("#Habilidades").css("color","#F1C40F");
+		$("#acerdade,#Educacion,#Portafolio,#Intereses,#Hablemos").css("color","white");
 	})
 
 	$("#Portafolio").on("click",function(e){
 		e.preventDefault();
 		animacion("#contenedorPortafolio");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Educacion,#Habilidades,#Intereses,#Hablemos").css("color","white");
 	})
 
 	$("#Educacion").on("click",function(e){
 		e.preventDefault();
 		animacion("#contenedorEducacion");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Habilidades,#Portafolio,#Intereses,#Hablemos").css("color","white");
 	})
 
 
 	$("#Intereses").on("click",function(e){
 		e.preventDefault();
 		animacion("#contenedorInteres");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Habilidades,#Portafolio,#Educacion,#Hablemos").css("color","white");
 	})
 
 	$("#Hablemos").on("click",function(e){
 		e.preventDefault();
 		animacion("#contenedorContacto");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Habilidades,#Portafolio,#Intereses,#Educacion").css("color","white");
 	})
 
-/*====================Configuracion del Menu pegajoso================*/
+	/*====================Configuracion del Menu pegajoso================*/
 
 	$( window ).scroll(function(){
     	var dato =  $(window).scrollTop();
 
-    	if (dato>560) {
-    		$("header").css({
+    	if (dato>10) {
+    	$("header").css({
     			"background-color":"black",
     			"display":"flex",
-    			"justify-content":"flex-end",
+    			"justify-content":"space-between",
     			"position": "fixed",
     			"z-index": "1000px",
     			"top":"0px"
     		});
+
+
     	}else{
     		$("header").css("display","none");
     	}
---
+++ Mi Pagina Web/JS/index.js
@@ -0,0 +1,71 @@
+
+ function animacion(contenedor){
+	 $('html, body').animate(
+	    {
+	      scrollTop: $(contenedor).offset().top
+	    },1000); 	
+ }
+
+
+$(document).ready(function(){ /*Bloque de codigo JQUERY*/
+
+/*==========Funcionamiento con animacion de los botones de la portada======*/
+	
+	$("#Habilidades").on("click",function(e){
+		e.preventDefault();
+		animacion("#contenedorHabilidades");
+		$("#Habilidades").css("color","#F1C40F");
+		$("#acerdade,#Educacion,#Portafolio,#Intereses,#Hablemos").css("color","white");
+	})
+
+	$("#Portafolio").on("click",function(e){
+		e.preventDefault();
+		animacion("#contenedorPortafolio");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Educacion,#Habilidades,#Intereses,#Hablemos").css("color","white");
+	})
+
+	$("#Educacion").on("click",function(e){
+		e.preventDefault();
+		animacion("#contenedorEducacion");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Habilidades,#Portafolio,#Intereses,#Hablemos").css("color","white");
+	})
+
+
+	$("#Intereses").on("click",function(e){
+		e.preventDefault();
+		animacion("#contenedorInteres");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Habilidades,#Portafolio,#Educacion,#Hablemos").css("color","white");
+	})
+
+	$("#Hablemos").on("click",function(e){
+		e.preventDefault();
+		animacion("#contenedorContacto");
+		$(this).css("color","#F1C40F");
+		$("#acerdade,#Habilidades,#Portafolio,#Intereses,#Educacion").css("color","white");
+	})
+
+	/*====================Configuracion del Menu pegajoso================*/
+
+	$( window ).scroll(function(){
+    	var dato =  $(window).scrollTop();
+
+    	if (dato>560) {
+    	$("header").css({
+    			"background-color":"rgba(0, 0, 0,0.5)",
+    			"display":"flex",
+    			"justify-content":"space-between;",
+    			"position": "fixed",
+    			"z-index": "1000px",
+    			"top":"0px"
+    		});
+
+
+    	}else{
+    		$("header").css("display","none");
+    	}
+
+  	});
+})
