afe442b5a34acc7f825db42154cf350b44b31d76
cristian clavijo <cristian982940@gmail.com>
2018-10-09 00:00:01 -0500

    Version 9/10/2018
    Se termino el contenedor portaafolio con sus enlaces direccionados y en sus modales con sus botones de redireccionamiento tanto html css y js con sus respectivos pantallazos
    
    Ademas se agrego un modal al boton de Acerca de, con un modal usando bootstrap

--- JS/index.js
+++ JS/index.js
@@ -6,23 +6,26 @@
 	    },1000); 	
  }
 
-
-function codeCss(idModal,nombreImagenCss){/*Funcion para los botones css y js de los modal del portafolio*/
+function codeHtml(idModal,carpeta,numeroImagen){/*Funcion para los botones css y js de los modal del portafolio*/
 	$(idModal + " .modal-body img").css("display","none"); /*Esconder las imagenes del html*/
-
-	$(idModal + " .modal-body").html('<img src="ImagenesPortafolio/CodigoFuente/Turismo/css/' +nombreImagenCss+ '.png" alt="">');/*Imagen nueva*/
-	$(idModal + " .modal-header .contenedor-btn-code .btn-info").css("display","none");/*Escondemos el boton css*/
-	$(idModal + " .modal-header .contenedor-btn-code").prepend('<button type="button" class="btn btn-primary" id="htmlTurismo">Html</button>'); /*Creamos un boton en el mismo lugar del boton del css*/
+	for(var i = 1; i<=numeroImagen; i++){
+		$(idModal + " .modal-body").append('<img src="ImagenesPortafolio/CodigoFuente/' + carpeta + '/html/imagen' + i + '.png" alt="">');/*Imagen nueva*/		
+	}
 }
 
-function codeJs(idModal,nombreImagenCss){/*Funcion para los botones css y js de los modal del portafolio*/
+function codeCss(idModal,carpeta,numeroImagen){/*Funcion para los botones css y js de los modal del portafolio*/
 	$(idModal + " .modal-body img").css("display","none"); /*Esconder las imagenes del html*/
-
-	$(idModal + " .modal-body").html('<img src="ImagenesPortafolio/CodigoFuente/Turismo/js/' +nombreImagenCss+ '.png" alt="">');/*Imagen nueva*/
-	$(idModal + " .modal-header .contenedor-btn-code .btn-warning").css("display","none");/*Escondemos el boton css*/
-	$(idModal + " .modal-header .contenedor-btn-code").prepend('<button type="button" class="btn btn-primary" id="htmlTurismo">Html</button>'); /*Creamos un boton en el mismo lugar del boton del css*/
+	for(var i = 1; i<=numeroImagen; i++){
+		$(idModal + " .modal-body").append('<img src="ImagenesPortafolio/CodigoFuente/' + carpeta + '/css/imagen' + i + '.png" alt="">');/*Imagen nueva*/		
+	}
 }
 
+function codeJs(idModal,carpeta,numeroImagen){/*Funcion para los botones css y js de los modal del portafolio*/
+	$(idModal + " .modal-body img").css("display","none"); /*Esconder las imagenes del html*/
+	for(var i = 1; i<=numeroImagen; i++){
+		$(idModal + " .modal-body").append('<img src="ImagenesPortafolio/CodigoFuente/' + carpeta + '/js/imagen' + i + '.png" alt="">');/*Imagen nueva*/		
+	}
+}
 
 $(document).ready(function(){ /*Bloque de codigo JQUERY*/
 
@@ -95,11 +98,110 @@ $(document).ready(function(){ /*Bloque de codigo JQUERY*/
 
   	/*Botones de los modales del portafolio css y js*/
 
-  	$("#css3Turismo").on("click",function(){
-  		codeCss("#myModal","imagen1");
+  	/*Botones del proyecto de Turismo en Colombia*/
+  	$("#htmlTurismo").on("click",function(){
+	  	codeHtml("#myModal","Turismo","5");  			
   	});
 
-  	$("#jsTurismo").on("click",function(){
-  		codeJs("#myModal","imagen");
+  	$("#css3Turismo").on("click",function(){
+	  	codeCss("#myModal","Turismo","2");  			
+  	});
+
+  	/*Botones del proyecto de Calculadora*/
+  	$("#htmlCal").on("click",function(){
+	  	codeHtml("#myModalCalculadora","calculadora","1");  			
+  	});
+
+  	$("#css3Cal").on("click",function(){
+	  	codeCss("#myModalCalculadora","calculadora","2");  			
+  	});
+
+  	$("#jsCal").on("click",function(){
+	  	codeJs("#myModalCalculadora","calculadora","7");  			
+  	});
+
+
+  	/*Botones del proyecto de Candy Crush*/
+  	$("#htmlCandy").on("click",function(){
+	  	codeHtml("#myModalCandy","candyCrush","1");  			
+  	});
+
+  	$("#css3Candy").on("click",function(){
+	  	codeCss("#myModalCandy","candyCrush","2");  			
+  	});
+
+  	$("#jsCandy").on("click",function(){
+	  	codeJs("#myModalCandy","candyCrush","4");  			
+  	});
+
+  	/*Botones del proyecto de Api Marvel*/
+  	$("#htmlMarvel").on("click",function(){
+	  	codeHtml("#myModalMarvel","marvel","2");  			
+  	});
+
+  	$("#css3Marvel").on("click",function(){
+	  	codeCss("#myModalMarvel","marvel","2");  			
+  	});
+
+  	$("#jsMarvel").on("click",function(){
+	  	codeJs("#myModalMarvel","marvel","2");  			
+  	});
+
+  	/*Botones del proyecto de comidas*/
+  	$("#htmlComidas").on("click",function(){
+	  	codeHtml("#myModalComidas","comidas","2");  			
+  	});
+
+  	$("#css3Comidas").on("click",function(){
+	  	codeCss("#myModalComidas","comidas","2");  			
+  	});
+
+  	/*Botones del proyecto del reloj de Iphone X*/
+
+  	$("#htmlIphone").on("click",function(){
+	  	codeHtml("#myModaliphone","reloj","1");  			
+  	});
+
+  	$("#css3Iphone").on("click",function(){
+	  	codeCss("#myModaliphone","reloj","2");  			
+  	});
+
+  	$("#jsIphone").on("click",function(){
+	  	codeJs("#myModaliphone","reloj","1");  			
+  	});
+
+  	/*Botones del proyecto de la tiena online*/
+  	$("#htmlTonline").on("click",function(){
+	  	codeHtml("#myModalTonline","tiendaOnline","4");  			
+  	});
+
+  	$("#css3Tonline").on("click",function(){
+	  	codeCss("#myModalTonline","tiendaOnline","2");  			
+  	});
+
+  	/*Botones del proyecto del juego de la granja*/
+  	 $("#htmlGranja").on("click",function(){
+	  	codeHtml("#myModalGranja","granja","2");  			
+  	});
+
+  	$("#cssGranja").on("click",function(){
+	  	codeCss("#myModalGranja","granja","3");  			
+  	});
+
+  	$("#jsGranja").on("click",function(){
+	  	codeJs("#myModalGranja","granja","3");  			
+  	});
+
+  	/*Botones del proyecto de Api Github*/
+  	$("#htmlGit").on("click",function(){
+	  	codeHtml("#myModalGit","github","1");  			
+  	});
+
+  	$("#cssGit").on("click",function(){
+	  	codeCss("#myModalGit","github","2");  			
+  	});
+
+  	$("#jsGit").on("click",function(){
+	  	codeJs("#myModalGit","github","1");  			
   	});
 })
